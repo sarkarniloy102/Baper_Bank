aa8b84af4d170b4267257637e55eb1f8cfc0759c
sarkarniloy102 <sarkarniloy102@gmail.com>
2023-08-16 20:25:11 +0600

    Add event listener and compare email password

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// add click event handler with the submit button
+document.getElementById('submit_btn').addEventListener('click', function () {
+    // to get email from emailfield
+    const email_field = document.getElementById('input_email');
+    const email = email_field.value;
+    // to get password from passwordfield
+    const pass_field = document.getElementById('input_pass');
+    const password = pass_field.value;
+
+    // DANGER: DO NOT VERIFY email password on the client side
+    // I am doing this now just to learn
+    if (email === 'sarkarniloy102@gmail.com' && password === 'Niloy102')
+        console.log('valid');
+    else
+        console.log('invalid');
+})
